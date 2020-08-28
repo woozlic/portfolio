790e8c21c37f34a9363e130e23bf9a3523bffca4
woozlic <sampaccs@mail.ru>
2020-08-28 20:54:15 +0500

    4th commit - added frontend, posts, button for random project

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
+import re
+
+app_name = 'main'
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
--- portfolio_site/urls.py
+++ portfolio_site/urls.py
@@ -18,5 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('portfolio.urls'))
+    path('', include('portfolio.urls')),
+    path('polls/', include('polls.urls')),
+    path('feedback/', include('write.urls'))
 ]
